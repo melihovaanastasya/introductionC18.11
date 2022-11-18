5520ab4c603c7a1ebea7cb87952ee5c47f09cc37
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-18 23:07:03 +0300

    30

---
+++ 30/Program.cs
@@ -0,0 +1,16 @@
+﻿// 30. Написать программу вычисления произведения чисел от 1 до N
+
+int Multip(int mul)
+{
+    int i=1;
+    int k=1;
+    while (i<=mul)
+    {
+        k=k*i;
+        i++;
+    }
+    return k;
+}
+
+int mul=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Multip(mul));
