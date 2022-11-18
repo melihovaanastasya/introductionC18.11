f602bd6faa468c4767d31a3f808d852a0a1fd882
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-18 21:11:40 +0300

    31

---
+++ 31/Program.cs
@@ -0,0 +1,14 @@
+﻿// 31. Сгенерировать 10 случайных чисел и показать кубы тех, которые заканчиваются на четную цифру.
+
+int r=10;
+double t=r;
+double x=23;
+int o=(int)x; 
+Random random=new Random(10);
+for (int i=0;i<5;i++)
+{
+    int a=random.Next(1,100);
+    int b=a; //(int)Math.Pow(a,3);
+    if (b%2==0)
+    System.Console.WriteLine($"{a}, {b}");
+}
