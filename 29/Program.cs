4d119f34e63d8b17ec6336603f933db6758751d9
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-18 23:00:56 +0300

    29

---
+++ 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// 29. Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+int Summary(int sum)
+{
+    int i=0;
+    while (sum>0)
+    {
+        i=i+sum%10;
+        sum=sum/10;
+    }
+    return i;
+}
+
+int sum=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Summary(sum));
