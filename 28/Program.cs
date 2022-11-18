1cd6cac1075871cbf17db0f4eaabaa792e001bd7
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-18 20:49:41 +0300

    28

---
+++ 28/Program.cs
@@ -0,0 +1,17 @@
+﻿// 28. Определить количество цифр в числе. Сделать подпрограмму.
+
+int CountDigits(int Number)
+{
+    Number=Math.Abs(Number);
+    int count=0;
+    if (Number==0) count=1;
+    while (Number>0)
+    {
+        Number=Number/10;
+        count++;
+    }
+    return count;
+}
+
+int Number=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(CountDigits(Number));
