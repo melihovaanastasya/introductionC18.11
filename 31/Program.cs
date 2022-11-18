863e19c4070eec3255778c90241e8f80cfe1961d
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-18 21:37:25 +0300

    evklid

--- 31/Program.cs
+++ 31/Program.cs
@@ -1,14 +1,16 @@
 ﻿// 31. Сгенерировать 10 случайных чисел и показать кубы тех, которые заканчиваются на четную цифру.
 
-int r=10;
-double t=r;
-double x=23;
-int o=(int)x; 
+bool Test(int a)
+{
+    //if (a%2==0) return true; else return false;
+    return a%2==0;
+}
+
 Random random=new Random(10);
-for (int i=0;i<5;i++)
+for (int i=0;i<20;i++)
 {
     int a=random.Next(1,100);
     int b=a; //(int)Math.Pow(a,3);
-    if (b%2==0)
+    if (Test(b))
     System.Console.WriteLine($"{a}, {b}");
 }
---
+++ evklid/Program.cs
@@ -0,0 +1,26 @@
+﻿// Алгоритм Евклида.
+
+/*
+int GCD(int a, int b)
+{
+while (a!=b)
+    if (a>b)
+        a=a-b;
+    else
+        b=b-a;
+return a;
+}
+System.Console.WriteLine(GCD(15,20));
+*/
+
+
+int QuickGCD(int a, int b)
+{
+while (a!=0 && b!=0)
+    if (a>b)
+        a=a%b;
+    else
+        b=b%a;
+return a+b;
+}
+System.Console.WriteLine(QuickGCD(15,20));
